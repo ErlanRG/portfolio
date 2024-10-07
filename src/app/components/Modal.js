295d8fba525cd0587b3c50688da04576f2c6547a
Erlan Rangel <erlanrangel@gmail.com>
2024-10-07 16:51:34 -0600

    fix: Project info button and go to repo icon

--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -5,7 +5,7 @@ const Modal = ({ title, children, href, modalId }) => {
     <>
       <button
         type="button"
-        className="btn bg-cat-maroon text-cat-base hover:bg-cat-red w-1/2"
+        className="btn bg-cat-maroon text-cat-base hover:bg-cat-red w-1/3"
         data-bs-toggle="modal"
         data-bs-target={`#${modalId}`}
       >
@@ -37,8 +37,15 @@ const Modal = ({ title, children, href, modalId }) => {
             </div>
             <div className="modal-footer">
               <Link href={href} passHref>
-                <button type="button" className="btn bg-cat-maroon hover:bg-cat-red">
-                  Go to repository
+                <button type="button" className="btn bg-cat-maroon hover:bg-cat-red flex justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M12 .5C5.373.5 0 5.873 0 12.5c0 5.245 3.388 9.688 8.06 11.245.59.109.8-.256.8-.569 0-.28-.01-1.21-.015-2.191-3.283.715-3.973-1.58-3.973-1.58-.537-1.364-1.312-1.73-1.312-1.73-1.075-.735.083-.72.083-.72 1.188.083 1.813 1.22 1.813 1.22 1.055 1.811 2.77 1.288 3.445.986.107-.765.414-1.288.754-1.588-2.665-.3-5.466-1.333-5.466-5.933 0-1.312.468-2.386 1.236-3.23-.124-.303-.535-1.528.117-3.176 0 0 1.008-.323 3.295 1.23.957-.267 1.987-.399 3.005-.404 1.018.005 2.048.137 3.005.404 2.287-1.553 3.295-1.23 3.295-1.23.653 1.648.241 2.873.118 3.176.769.844 1.236 1.918 1.236 3.23 0 4.608-2.807 5.624-5.471 5.914.426.367.807 1.096.807 2.206 0 1.592-.014 2.874-.014 3.26 0 .314.209.684.805.568C20.612 22.188 24 17.745 24 12.5 24 5.873 18.627.5 12 .5z" />
+                  </svg>
                 </button>
               </Link>
             </div>
--- src/app/components/Project.js
+++ src/app/components/Project.js
@@ -6,7 +6,7 @@ const Project = ({ img, title, description, href, details, skills }) => {
   const modalId = `modal-${title.replace(/\s+/g, '-').toLowerCase()}`;
 
   return (
-    <div className="rounded-lg shadow-xl overflow-hidden w-full max-w-md mx-auto">
+    <div className="rounded-lg shadow-xl overflow-hidden w-full max-w-md mx-auto flex flex-col justify-between">
       <Image
         src={img}
         alt={title}
@@ -14,13 +14,17 @@ const Project = ({ img, title, description, href, details, skills }) => {
         height={250}
         className="object-cover w-full h-48"
       />
-      <div className="p-4 text-center flex flex-col gap-3 align-items-center">
+      <div className="p-4 text-center flex flex-col gap-3">
         <h2 className="text-cat-maroon text-lg font-bold">{title}</h2>
         <p className="text-cat-text">{description}</p>
+      </div>
+      <div className="flex justify-center mb-4">
         <Modal title={title} href={href} modalId={modalId}>
-          <p className="text-cat-text">{details}</p>
-          <h1 className="text-cat-text pt-3">Skills:</h1>
-          <SkillList skills={skills} />
+          <div className="text-center">
+            <p className="text-cat-text">{details}</p>
+            <h1 className="text-cat-text pt-3">Skills:</h1>
+            <SkillList skills={skills} />
+          </div>
         </Modal>
       </div>
     </div>
